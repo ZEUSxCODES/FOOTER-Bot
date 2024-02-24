a4b97bfb2cad66dce15bd194625f33d1842f9d4c
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-24 12:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 
 class Config(object):
     MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("APP_ID", 20937822))
+    API_HASH = os.environ.get("API_HASH", "68d3b463d3c53536782545f790aa5147")
     CAPTION = os.environ.get("CAPTION", "")
     BUTTON_TEXT = os.environ.get("BUTTON", "")
     URL_LINK = os.environ.get("LINK", "")
